15a95f88b1978fc2a75daf40158e1b0e07f29791
Matheus <mvaz41322@gmail.com>
2026-05-19 16:44:46 -0300

    main atualizada

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,26 @@ public class Main {
         arvoreBinaria.inserir(7);
         arvoreBinaria.inserir(9);
         arvoreBinaria.exibir("Pos");
+        System.out.println();
 
+        // removendo nó folha
+        arvoreBinaria.remover(-4);
+        System.out.println("Árvore após remoção do nó folha");
+        arvoreBinaria.exibir("Em");
+        System.out.println();
+
+        // removendo nó com um filho
+        arvoreBinaria.remover(10);
+        System.out.println("Árvore após remoção do nó com um filho");
+        arvoreBinaria.exibir("Em");
+        System.out.println();
+
+        //removendo nó com dois filhos
+        arvoreBinaria.remover(5);
+        System.out.println("Árvore após remoção do nó com dois filhos");
+        arvoreBinaria.exibir("Em");
+        System.out.println();
+
+        arvoreBinaria.exibir("Pós");
     }
 }
